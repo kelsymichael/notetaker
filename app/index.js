e68e87f848c1c61fa658562c536d9aee428bdf27
kelsy michael gagnebin <kelsymichael@gmail.com>
2017-01-23 15:26:47 -0700

    Update component name

--- app/index.js
+++ app/index.js
@@ -1,2 +1,15 @@
-var app = document.getElementById('app');
-app.innerHTML = 'hello'
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Header = React.createClass({
+  render: function(){
+    return (
+        <div> hello world </div>
+      )
+  }
+});
+
+ReactDOM.render(
+  <Header />,
+  document.getElementById('app')
+);
