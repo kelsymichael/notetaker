bdc29d61eff1d62a2a34cdda302991f09f9d21ec
kelsy michael gagnebin <kelsymichael@gmail.com>
2017-01-27 14:22:42 -0700

    Add nested components

--- app/index.js
+++ app/index.js
@@ -1,6 +1,26 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var Footer = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <h1> imma footer </h1>
+      </div>
+      )
+  }
+});
+
+var Middle = React.createClass({
+  render: function(){
+    return (
+        <div>
+          <h4> imma middle content </h4>
+        </div>
+      )
+  }
+});
+
 var Header = React.createClass({
   render: function(){
     return (
@@ -9,7 +29,19 @@ var Header = React.createClass({
   }
 });
 
+var Container = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <Header />
+        <Middle />
+        <Footer />
+      </div>
+      )
+  }
+});
+
 ReactDOM.render(
-  <Header />,
+  <Container />,
   document.getElementById('app')
 );
